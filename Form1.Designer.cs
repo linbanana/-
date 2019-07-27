42a8e170482024958395c1236f14473175c07a2d
linbanana <tommy12345689@yahoo.com.tw>
2019-07-28 00:14:57 +0800

    2019/07/28

---
+++ Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace 巴那那的抽籤程式
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.start = new System.Windows.Forms.Button();
+			this.label4 = new System.Windows.Forms.Label();
+			this.Enter1 = new System.Windows.Forms.Button();
+			this.Enter2 = new System.Windows.Forms.Button();
+			this.output = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F);
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(90, 665);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(212, 32);
+			this.label1.TabIndex = 1;
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.checkBox1.ForeColor = System.Drawing.Color.White;
+			this.checkBox1.Location = new System.Drawing.Point(22, 673);
+			this.checkBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(61, 20);
+			this.checkBox1.TabIndex = 2;
+			this.checkBox1.Text = "顯示IP";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("微軟正黑體", 10F);
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(533, 665);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(196, 32);
+			this.label2.TabIndex = 3;
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.Color.Gray;
+			this.textBox1.Enabled = false;
+			this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.textBox1.ForeColor = System.Drawing.Color.Black;
+			this.textBox1.Location = new System.Drawing.Point(1, 121);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(146, 23);
+			this.textBox1.TabIndex = 4;
+			this.textBox1.Text = "請輸入人數";
+			this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.BackColor = System.Drawing.Color.Gray;
+			this.richTextBox1.Enabled = false;
+			this.richTextBox1.Font = new System.Drawing.Font("微軟正黑體", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.richTextBox1.ForeColor = System.Drawing.Color.Black;
+			this.richTextBox1.Location = new System.Drawing.Point(155, 100);
+			this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(292, 450);
+			this.richTextBox1.TabIndex = 5;
+			this.richTextBox1.Text = "按Enter後為下一筆資料";
+			this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("微軟正黑體", 12F);
+			this.button1.Location = new System.Drawing.Point(37, 61);
+			this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(71, 31);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "一般";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("微軟正黑體", 12F);
+			this.button2.Location = new System.Drawing.Point(256, 61);
+			this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(71, 31);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "自訂";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label3
+			// 
+			this.label3.Cursor = System.Windows.Forms.Cursors.Default;
+			this.label3.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label3.ForeColor = System.Drawing.SystemColors.Control;
+			this.label3.Location = new System.Drawing.Point(47, -3);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(255, 44);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "請選擇抽籤方式";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.label3.Click += new System.EventHandler(this.label3_Click);
+			// 
+			// start
+			// 
+			this.start.Font = new System.Drawing.Font("微軟正黑體", 15F);
+			this.start.Location = new System.Drawing.Point(1, 600);
+			this.start.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.start.Name = "start";
+			this.start.Size = new System.Drawing.Size(446, 44);
+			this.start.TabIndex = 9;
+			this.start.Text = "開始抽籤";
+			this.start.UseVisualStyleBackColor = true;
+			this.start.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("微軟正黑體", 20F);
+			this.label4.ForeColor = System.Drawing.Color.White;
+			this.label4.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+			this.label4.Location = new System.Drawing.Point(453, 426);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(441, 206);
+			this.label4.TabIndex = 10;
+			this.label4.Click += new System.EventHandler(this.label4_Click);
+			// 
+			// Enter1
+			// 
+			this.Enter1.Font = new System.Drawing.Font("微軟正黑體", 12F);
+			this.Enter1.Location = new System.Drawing.Point(1, 166);
+			this.Enter1.Name = "Enter1";
+			this.Enter1.Size = new System.Drawing.Size(146, 30);
+			this.Enter1.TabIndex = 11;
+			this.Enter1.Text = "確認";
+			this.Enter1.UseVisualStyleBackColor = true;
+			this.Enter1.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// Enter2
+			// 
+			this.Enter2.Font = new System.Drawing.Font("微軟正黑體", 15F);
+			this.Enter2.Location = new System.Drawing.Point(155, 557);
+			this.Enter2.Name = "Enter2";
+			this.Enter2.Size = new System.Drawing.Size(292, 36);
+			this.Enter2.TabIndex = 12;
+			this.Enter2.Text = "確認";
+			this.Enter2.UseVisualStyleBackColor = true;
+			this.Enter2.Click += new System.EventHandler(this.Enter2_Click);
+			// 
+			// output
+			// 
+			this.output.Font = new System.Drawing.Font("微軟正黑體", 40F);
+			this.output.ForeColor = System.Drawing.Color.White;
+			this.output.Location = new System.Drawing.Point(453, 151);
+			this.output.Name = "output";
+			this.output.Size = new System.Drawing.Size(429, 240);
+			this.output.TabIndex = 13;
+			this.output.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.output.Click += new System.EventHandler(this.output_Click);
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("微軟正黑體", 40F);
+			this.label5.ForeColor = System.Drawing.Color.White;
+			this.label5.Location = new System.Drawing.Point(470, 35);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(412, 109);
+			this.label5.TabIndex = 14;
+			this.label5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+			this.label5.Click += new System.EventHandler(this.label5_Click);
+			// 
+			// label6
+			// 
+			this.label6.BackColor = System.Drawing.Color.Black;
+			this.label6.Font = new System.Drawing.Font("微軟正黑體", 12F);
+			this.label6.ForeColor = System.Drawing.Color.Yellow;
+			this.label6.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.label6.Location = new System.Drawing.Point(701, 673);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(202, 24);
+			this.label6.TabIndex = 15;
+			this.label6.Text = "巴那那  ©  2016-2019";
+			this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.label6.Click += new System.EventHandler(this.Label6_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.ClientSize = new System.Drawing.Size(894, 699);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.output);
+			this.Controls.Add(this.Enter2);
+			this.Controls.Add(this.Enter1);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.start);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.checkBox1);
+			this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "巴那那的抽籤程式";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Enter1;
+        private System.Windows.Forms.Button Enter2;
+        private System.Windows.Forms.Label output;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 巴那那的抽籤程式
+{
+    public partial class Form1 : Form
+    {
+        //private IPHostEntry IPHost = Dns.GetHostEntry(Environment.MachineName);
+        int x = 0;
+        private List<int> alist;
+        private Random r = new Random(Guid.NewGuid().GetHashCode());
+        private string[] ax;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            string processName = Process.GetCurrentProcess().ProcessName;
+            Process[] processesByName = Process.GetProcessesByName(processName);
+            if (processesByName.Length > 1)
+            {
+                MessageBox.Show("程式重複執行", "錯誤");
+                Environment.Exit(2);
+            }
+            label2.Text = Convert.ToString(DateTime.Now);
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+				//label1.Text = "本機電腦IP：" + this.IPHost.AddressList[1].ToString();
+				MessageBox.Show("此功能暫時關閉", "Error");
+			}
+            else
+            {
+				//label1.Text = "";
+				MessageBox.Show("此功能暫時關閉", "Error");
+			}
+                
+            
+        }
+
+        private void toolStripContainer1_TopToolStripPanel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text = Convert.ToString(DateTime.Now);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ax = null;
+            textBox1.Enabled = true;
+            textBox1.BackColor = System.Drawing.ColorTranslator.FromHtml("#FFFFFF");
+            richTextBox1.Enabled = false;
+            richTextBox1.BackColor = System.Drawing.ColorTranslator.FromHtml("#808080");
+            textBox1.Clear();                     
+            output.Text = "";
+            label4.Text = "";
+            richTextBox1.Text = "按Enter為下一行";
+            label5.Text = "";
+            textBox1.Focus();
+
+
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            alist = null;
+            richTextBox1.Enabled = true;
+            richTextBox1.BackColor = System.Drawing.ColorTranslator.FromHtml("#FFFFFF");
+            textBox1.Enabled = false;
+            textBox1.BackColor = System.Drawing.ColorTranslator.FromHtml("#808080");
+            textBox1.Clear();
+            label5.Text = "";
+            output.Text = "";
+            label4.Text = "";
+            textBox1.Text = "請輸入人數";
+            richTextBox1.Clear();
+            richTextBox1.Focus();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Int32 lineLength = richTextBox1.Lines.Length;
+            if (lineLength > 0)
+            {
+                label5.Text = "共有" + Convert.ToString(lineLength-1) + "筆資料";
+            } 
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Enabled ==false & richTextBox1.Enabled==false)
+            {
+                MessageBox.Show("請先選擇抽籤方式");
+                return;
+            }
+            //一般抽籤
+            if (alist != null && alist.Count == 0)
+            {
+                try
+                {
+
+                    int index = r.Next(alist.Count);
+                    label5.Text = "我抽我抽我抽抽抽";
+                    output.Text = Convert.ToString(alist[index]);
+                    alist.RemoveAt(index);
+                    label4.Text = string.Concat(new object[]
+                    {
+                    label4.Text=output.Text,"已被移除，目前剩",alist.Count,"位"
+                    });
+                    if (this.alist.Count == 0)
+                    {
+                        MessageBox.Show("已無數字");
+                        alist.Clear();
+                        alist = null;
+                        textBox1.Clear();
+                        label5.Text = "";
+                        output.Text = "";                        
+                        label4.Text = "";
+                        MessageBox.Show("欄位已全清空", "提示");
+                        this.textBox1.Focus();
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("請輸入數字後按確認", "Error");
+                }
+            }
+            else
+            {
+                //自訂抽籤
+               
+                try
+                 {
+
+
+					label5.Text = "我抽我抽我抽抽抽";
+					int index = r.Next(0, ax.Length);
+					output.Text = Convert.ToString(ax[index]);
+					ax = ax.Where(val => val != ax[index]).ToArray();
+					label4.Text = string.Concat(new object[]
+                                            {
+                    label4.Text=output.Text,"已被移除，目前剩",ax.Length,"位"
+                                            });
+                    if (ax.Length == 0)
+                     {
+						MessageBox.Show("已無資料");
+
+						richTextBox1.Clear();
+						ax = null;
+						label5.Text = "";
+						label4.Text = "";
+						output.Text = "";
+						MessageBox.Show("欄位已全清空", "提示");
+						richTextBox1.Focus();
+				     }
+
+
+                 }
+                 catch (Exception)
+                 {
+                   MessageBox.Show("請輸入資料後按確認", "Error");
+                 }
+                
+            }
+            
+
+            
+            
+
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Enabled == false & richTextBox1.Enabled == false)
+            {
+                MessageBox.Show("請先選擇抽籤方式");
+                return;
+            }
+            try
+            {
+                x = Convert.ToInt32(textBox1.Text);
+                if (x <= 0)
+                {
+                    MessageBox.Show("數字不能為0");
+                    textBox1.Clear();
+                    return;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            if (alist==null)
+            {
+                   MessageBox.Show("請重新輸入數字");
+                    textBox1.Clear();                    
+                   textBox1.Focus();
+            }
+
+            }
+            this.alist = new List<int>();
+            for (int i = 1; i <= this.x; i++)
+            {
+                this.alist.Add(i);
+            }
+        }
+
+        private void Enter2_Click(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Enabled == false & richTextBox1.Enabled == false)
+            {
+                MessageBox.Show("請先選擇抽籤方式");
+                return;
+            }            
+            //一行取一個按Enter一行
+            string str = richTextBox1.Text.Trim().Replace(System.Environment.NewLine, "\n");
+            ax = str.Split('\n');
+            
+            
+
+
+
+
+
+        }
+
+        private void output_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+		private void Label6_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
